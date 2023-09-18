01f4f3b0fe26a326463586294230e1a1f0cdfcd1
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-09-18 14:29:36 +0200

    First try :)

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 [![Build Status](https://github.com/your-GitHub-username/MeanFieldSim.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/your-GitHub-username/MeanFieldSim.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/your-GitHub-username/MeanFieldSim.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/your-GitHub-username/MeanFieldSim.jl)
+
+This package implements a neural network based simulation method for a mean field game.
--- src/MeanFieldSim.jl
+++ src/MeanFieldSim.jl
@@ -1,5 +1,31 @@
 module MeanFieldSim
 
-# Write your package code here.
+struct MeanFieldGame
+
+c::Float64
+γ::Float64
+
+end
+
+"""
+This implements sampling from a n-dimensional brownian motion.
+"""
+B(n, t) = ...
+
+"""
+samples ?
+"""
+function α(t)
+    return exp(γ*B(2, t) - γ^2*t/2)
+end
+
+# maybe doing this iteratively and using only vector/scalar might be sufficient
+function loss(v, ε::Matrix{T, 2}, y::Matrix{T, 1}) where {T <: Number}
+    x = 0.0
+    for i in axes(ε, 2) # loop over N columns
+        x += (v(ε[:, i]) - y[i])^2
+    end
+    return x
+end
 
 end
