69bf8c5cbc40dfeb3b0e4e9243e9d9c306ddb44c
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-09-18 15:03:21 +0200

    some progress

--- src/MeanFieldSim.jl
+++ src/MeanFieldSim.jl
@@ -1,10 +1,11 @@
 module MeanFieldSim
 
 struct MeanFieldGame
-
-c::Float64
-γ::Float64
-
+    c::Float64
+    γ::Float64
+    ρ::Float64
+    T::Float64
+    λ::Float64
 end
 
 """
@@ -26,6 +27,28 @@ function loss(v, ε::Matrix{T, 2}, y::Matrix{T, 1}) where {T <: Number}
         x += (v(ε[:, i]) - y[i])^2
     end
     return x
+end
+
+function update_sdf(ξ, α, η)
+    ξ .*= (1 - α)
+    η .+= α * η
+end
+
+function approximate(game::MeanFieldGame; N=100; p=10, iterations=100)
+    ξ = ones(N)
+    V = Vector{Float64}
+    η = Vector{Float64}
+
+    for q in 1:iterations
+        step_size = 2/(p + q)
+
+
+        # mehrere epochen, mache backpropagation auf batch aus N samples
+        # train schritte geben uns vₖ
+        update_value()
+        update_η()
+        update_sdf(ξ, step_size, η)
+    
 end
 
 end
