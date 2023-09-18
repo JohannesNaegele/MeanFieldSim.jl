631ce70ee0d26e28efd8c7865f8a59764c91909d
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-09-18 17:34:50 +0200

    struct should be ready

--- src/MeanFieldSim.jl
+++ src/MeanFieldSim.jl
@@ -1,15 +1,30 @@
 module MeanFieldSim
 
-struct MeanFieldGame{T}
-    c::T # emission efficiacy
-    γ::T # risk aversion parameter
-    ρ::T # proportion of green investors
-    T::T # time horizon
-    λ::T # environmental concern
-    μ::T # drift of value
-    σ::T # volatility of common risk
-    V₀::T # average initial firm value
+Base.@kwdef struct MeanFieldGame{S}
+    C::S # emission efficiacy
+    C₂::S # emission efficiacy ???
+    γ::S # risk aversion parameter
+    γ_star::S # risk aversion parameter ???
+    ρ::S # proportion of green investors
+    T::S # time horizon
+    λ::S # environmental concern
+    μ::S # drift of value
+    σ::S # volatility of common risk
+    V₀::S # average initial firm value
 end
+
+game = MeanFieldGame(
+    γ_star=0.5,
+    σ=0.1,
+    γ=0.3,
+    μ=0.05,
+    V₀=1.0,
+    C=0.7,
+    C₂=1.0,
+    λ=0.0,
+    ρ=0.0,
+    T=5.0
+)
 
 """
 This implements sampling from a n-dimensional brownian motion.
@@ -36,14 +51,13 @@ function loss(v, ε::Matrix{T, 2}, y::Matrix{T, 1}) where {T <: Number}
     return x
 end
 
-function update_value(game, V, E, v, ε) # v is a vector of neuronal networks
-    h = game.T/n
+function update_value(game, V, E, v, ε, h) # v is a vector of neuronal networks
     V .= game.V₀ * E[1, :]
     for i in eachindex(V)
         x = 0.0
         for k in axes(E, 2)
-            wₖ = (k == 1) || (k == size(E)[2]) ? 1.0/2.0 : 1
-            x += wₖ * game.c^2 * 1/α(t, k*h, h, ε[:, i, 2]) * E[k, i] * v[k](E[1:k, i])
+            wₖ = (k == 1) || (k == size(E)[1]) ? 1.0/2.0 : 1.0
+            x += wₖ * game.C₂^2 * 1/α(k*h, γ, h, ε[1:k, i, 2]) * E[k, i] * v[k](ε[1:k, i, :]) # TODO: check dimension with NN
         end
         V[i] += h * x
     end
@@ -51,14 +65,14 @@ end
 
 function update_η(game, η, h, ε)
     for i in eachindex(η)
-        η[i] = exp(-game.γ * V + game.ρ*(game.λ * sqrt(h) * sum(ε[:, i, 1]) - λ^2 * T/2))
+        η[i] = exp(-game.γ_star * V[i] + game.ρ*(game.λ * sqrt(h) * sum(ε[:, i, 2]) - λ^2 * game.T/2))
     end
     η[i] ./= sum(η)
 end
 
 function update_sdf(ξ, α, η)
     ξ .*= (1 - α)
-    η .+= α * η
+    ξ .+= α * η
 end
 
 function approximate(game::MeanFieldGame; n=100, k=0.1, N=100, p=10, iterations=100)
@@ -68,14 +82,17 @@ function approximate(game::MeanFieldGame; n=100, k=0.1, N=100, p=10, iterations=
     V = Vector{Float64}
     η = Vector{Float64}
 
+    h = game.T / n
+
     for q in 1:iterations
         step_size = 2/(p + q)
 
-
         # mehrere epochen, mache backpropagation auf batch aus N samples
+        v = ...
         # train schritte geben uns vₖ
-        update_value()
-        update_η()
+
+        update_value(game, V, E, v, ε, h)
+        update_η(game, η, h, ε)
         update_sdf(ξ, step_size, η)
     end
 end
