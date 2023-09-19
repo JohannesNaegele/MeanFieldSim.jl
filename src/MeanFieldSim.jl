5c8fcfe3028aa88c68afab6a9128e3c3ca2d5b61
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-09-19 18:36:48 +0200

    Funkt einigermaßen

--- src/MeanFieldSim.jl
+++ src/MeanFieldSim.jl
@@ -1,5 +1,9 @@
 module MeanFieldSim
 
+export MeanFieldGame
+export approximate
+
+using Statistics
 using Flux
 
 Base.@kwdef struct MeanFieldGame{S}
@@ -23,9 +27,10 @@ function update_value(game, V, E, v, ε, h) # v is a vector of neuronal networks
     V .= game.V₀ * E[1, :]
     for i in eachindex(V)
         x = 0.0
-        for k in axes(E, 2)
+        for k in axes(E, 1)
             wₖ = (k == 1) || (k == size(E)[1]) ? 1.0/2.0 : 1.0
-            x += wₖ * game.C₂^2 * 1/α(k*h, γ, h, ε[1:k, i, 2]) * E[k, i] * v[k](ε[1:k, i, :]) # TODO: check dimension with NN
+            # println(v[k](ε[1:k, i, :]))
+            x += wₖ * game.C₂^2 * 1/α(k*h, game.γ, h, ε[1:k, i, 2]) * E[k, i] * v[k](ε[1:k, i, :]) # TODO: check dimension with NN
         end
         V[i] += h * x
     end
@@ -52,30 +57,30 @@ end
 #     return x
 # end
 
-function approximate(game::MeanFieldGame; n=20, N=50000, p=2, iterations=100, epochs=3)
+function approximate(game::MeanFieldGame; n=20, N=50000, p=2, iterations=10, epochs=1)
     # FIXME: Summen k=0???
     ε = randn(n, N, 2)
     E = Matrix{Float64}(undef, n, N)
 
     h = game.T / n
 
-    for i in axes(E, 1)
-        for k in axes(E, 2)
+    for i in axes(E, 2)
+        for k in axes(E, 1)
             E[k, i] = exp(game.σ * sum(ε[1:k, i, 1]) + (game.μ - game.σ^2/2)* h * k)
         end
     end
 
-    ξ = ones(N)
-    V = Vector{Float64}
-    η = Vector{Float64}
+    ξ = ones(N) # stochastic discount factor
+    V = zeros(N)
+    η = zeros(N)
     # https://stats.stackexchange.com/a/136542/297734
     n_hidden(n_data, in, α=3.0) = Int(round(n_data/(α * (1 + in))/32)*32)
-
+    
     optimizer = Adam()
     v = [
             Chain(
-                Dense(k => n_hidden(N, 2*n), leakyrelu),   # activation function inside layer
-                Dense(n_hidden(N, 2*n) => 1),
+                Dense(2k => n_hidden(N, 2*n, 8.0), leakyrelu),   # activation function inside layer
+                Dense(n_hidden(N, 2*n, 8.0) => 1),
                 Dropout(0.5),
                 leakyrelu
             )
@@ -84,18 +89,21 @@ function approximate(game::MeanFieldGame; n=20, N=50000, p=2, iterations=100, ep
 
     for q in 1:iterations
         step_size = 2/(p + q)
-
         # mehrere epochen, mache backpropagation auf batch aus N samples
         for k in eachindex(v)
-            data = [ε[k, :, :], E[k, :] .* ξ[i]] # very inperformant
+            data = [(vec(ε[1:k, i, :]), E[k, i] * ξ[i]) for i in eachindex(ξ)] # very inperformant
+            println("q: $q, k: $k")
             for _ in 1:epochs
-                model = Flux.setup(v[i], optimizer)
-                Flux.train!((m,x,y) -> (m(x) - y)^2, model, data, optim)
+                opt = Flux.setup(optimizer, v[k])
+                # Flux.Losses.mse
+                # loss(ŷ, y, agg=x->mean(w .* x))
+                # TODO: batch size
+                Flux.train!((m,x,y) -> (mean(m(x) .- y).^2), v[k], data, opt)
             end
         end
         # train schritte geben uns vₖ
 
-        update_value(game, V, E, v, ε, h)
+        update_value(game, V, E, [x -> v[i](x)[1] for i in eachindex(v)], ε, h)
         update_η(game, η, h, ε)
         update_sdf(ξ, step_size, η)
     end
